ccbc8fbf8fe4808239f6607afa37767d9399d699
dominoma <dominoma@users.noreply.github.com>
2022-02-22 00:36:40 +0000

    deploy: openvideo-streaming/website@f13368a8c934f14a64db5d310de13b0892f48b50

--- 3.x.x/tracking/readonly-tracking.js
+++ 3.x.x/tracking/readonly-tracking.js
@@ -1,4 +1,5 @@
 const ga = window.ga
-Object.freeze(ga)
-delete window.ga
-Object.defineProperty(window, 'ga', { value: ga })
+        Object.freeze(ga)
+        delete window.ga
+        Object.defineProperty(window, 'ga', { value: ga })
+      
